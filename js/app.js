7bc75b9abab4a5ea3e5f2bded5e3eedb2c9dea3b
Awwal <awwallawal@gmail.com>
2024-10-17 17:30:15 +0100

    The slider autoplay is activated

--- js/app.js
+++ js/app.js
@@ -11,10 +11,10 @@ menuBar.addEventListener ("click", ()=>{
       spaceBetween: 30,
       centeredSlides: true,
       loop:true,
-      // autoplay: {
-      //   delay: 2500,
-      //   disableOnInteraction: false,
-      // },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
