a72149e9d267cda5b4d6f2606d8099dafa6a5ec1
Awwal <awwallawal@gmail.com>
2024-10-17 15:07:37 +0100

    Worked on the Hero sections to make the sliders functional. Need to make the page responsive to the 360px screen size which seems to the Andriod 10 screen pixel size

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
-// Navbar Toggle 
 let navBar = document.querySelector (".navLink");
 let menuBar = document.getElementById("menuBtn");
 menuBar.addEventListener ("click", ()=>{
     navBar.classList.toggle('active');
 });
 
+// Swiper JS 
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      loop:true,
+      // autoplay: {
+      //   delay: 2500,
+      //   disableOnInteraction: false,
+      // },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  });
